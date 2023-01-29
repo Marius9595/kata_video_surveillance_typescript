58c5df1fe8c470263fab62fcec1875d2ac63b195
Mario Pinto <mario@leanmind.es>
2023-01-29 12:12:39 +0000

    🟢 Video surveillance should start video recording when motion sensor is activated

--- src/core/video_surveillance_controller.ts
+++ src/core/video_surveillance_controller.ts
@@ -1,8 +1,12 @@
 import { CustomClock } from './custom_clock';
 
 export class VideoSurveillanceController {
-	constructor(private motion_sensor: MotionSensor, private vide_recorder: VideoRecorder, custom_clock: CustomClock) {}
+	constructor(private motion_sensor: MotionSensor, private video_recorder: VideoRecorder, custom_clock: CustomClock) {}
 
-	public start_surveillance() {}
+	public start_surveillance() {
+		if (this.motion_sensor.isDetectingMotion()) {
+			this.video_recorder.startRecording();
+		}
+	}
 	public stop_surveillance() {}
 }
